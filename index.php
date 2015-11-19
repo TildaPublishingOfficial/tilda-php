086e8341973fe6b7808babef66172b45964d3669
Michael Akimov <michael@island-future.ru>
2015-11-19 16:10:17 +0300

    bugfix

--- index.php
+++ index.php
@@ -1,10 +1,21 @@
 <?php
-include $_SERVER['DOCUMENT_ROOT'].DIRECTORY_SEPARATOR.'classes'.DIRECTORY_SEPARATOR.'Tilda.php';
+include $_SERVER['DOCUMENT_ROOT'].DIRECTORY_SEPARATOR.'config.php';
 
 //здесь проверяем, может есть статья с таким названием
 if (substr($_SERVER['SCRIPT_NAME'],-1) == '/') {
-    $articleName = substr($_SERVER['SCRIPT_NAME'],1,-1);
+    $pagename = substr($_SERVER['SCRIPT_NAME'],1,-1);
 } else {
-    $articleName = substr($_SERVER['SCRIPT_NAME'],1);
+    $pagename = substr($_SERVER['SCRIPT_NAME'],1);
 }
-return Tilda::showpage($articleName);
+
+$tilda = new Tilda(
+    $TILDA_PUBLIC_KEY,
+    $TILDA_SECRET_KEY,
+    array(
+        'baseDir' => $TILDA_BASE_DIR,
+        'projectDir' => $TILDA_PROJECT_DIR,
+        'buglovers' => $EMAIL_FOR_BUGS
+    )
+);
+
+return $tilda->showpage($pagename);
